91499deb2339e8f3fcb311fb10afc560f6037a18
Sachin Kumar <sachinjnv706@gmail.com>
2021-05-21 10:25:53 +0530

    improved code quality

--- src/workspace/Components/Question.js
+++ src/workspace/Components/Question.js
@@ -5,7 +5,7 @@ import Option from '../../components/Option'
 import Divider from '../../components/Divider'
 import Message from '../../components/Message'
 import Progress from '../../components/Progress'
-import {FaCheckCircle, FaAngleDoubleRight, FaAngleDoubleLeft, FaTimesCircle} from "react-icons/fa";
+import {FaCheckCircle, FaTimesCircle} from 'react-icons/fa'
 
 export class Question extends Component {
     constructor(props) {
@@ -13,12 +13,24 @@ export class Question extends Component {
         const UsersInput = props.questions.map(() => null);
         const InputLocked = props.questions.map(() => false);
         this.state = {
-            InputLocked: InputLocked,
+            CurrentQuestionNumber: 0,
             UsersInput: UsersInput,
-            CurrentQuestionNumber: 0
+            InputLocked: InputLocked
         }
-        this.SubmitHandler = this.SubmitHandler.bind(this);
+
+        // ui = [null, null, null, null, null, null]
+        // il = [false, false, false, false, false, flase]
+        // cqn = 0
+
+        // clickhandler
+        // if(il[cqn]===false) ui[cqn]=option;
+        // else return;
+        
+        // submithandler
+        // cqn++
+
         this.ClickHandler = this.ClickHandler.bind(this);
+        this.SubmitHandler = this.SubmitHandler.bind(this);
         this.GoToNextQuestion = this.GoToNextQuestion.bind(this);
         this.GoToPreviousQuestion = this.GoToPreviousQuestion.bind(this);
     }
@@ -30,67 +42,47 @@ export class Question extends Component {
         this.setState((state, props) => {
             let arr = state.UsersInput;
             arr[state.CurrentQuestionNumber] = option.id;
-            return {
-                UsersInput: arr
-            }
+            return {UsersInput: arr}
         });
         // console.log(label);
-        // console.log(option);
     }
 
     SubmitHandler(e) {
         this.setState((state, props) => {
             let arr = state.InputLocked;
             arr[state.CurrentQuestionNumber] = true;
-            return {
-                InputLocked: arr
-            }
-        }, () => {
-            console.log(this.state.UsersInput);
-            console.log(this.state.InputLocked);
+            return {InputLocked: arr}
         })
         // const input = this.state.UsersInput[this.state.CurrentQuestionNumber];
         // const answer = this.props.questions[this.state.CurrentQuestionNumber].answer;
-        // if(input===answer.id) {
-        //     console.log('Correct Answer');
-        // } else {
-        //     console.log('Wrong Answer');
-        // }
-        // console.log(input);
     }
 
     GoToNextQuestion() {
-        if(this.state.CurrentQuestionNumber >= 5) {
-            return;
-        }
+        if(this.state.CurrentQuestionNumber >= this.props.questions.length-1) return;
         this.setState((state, props) => {
-            return {
-                CurrentQuestionNumber: state.CurrentQuestionNumber + 1,
-
-            }
+            return {CurrentQuestionNumber: state.CurrentQuestionNumber + 1}
         })
     }
 
     GoToPreviousQuestion() {
-        if(this.state.CurrentQuestionNumber <= 0) {
-            return;
-        }
+        if(this.state.CurrentQuestionNumber <= 0) return;
         this.setState((state, props) => {
-            return {
-                CurrentQuestionNumber: state.CurrentQuestionNumber - 1
-            }
+            return {CurrentQuestionNumber: state.CurrentQuestionNumber - 1}
         })
     }
 
     render() {
-        const question = this.props.questions[this.state.CurrentQuestionNumber];
-        const numberOfQuestions = this.props.questions.length;
+        const cqn = this.state.CurrentQuestionNumber;
+        const question = this.props.questions[cqn];
+        const totalQuestions = this.props.questions.length;
+        const isInputLocked = this.state.InputLocked[cqn];
+        const usersInput = this.state.UsersInput[cqn];
         return (
             <Segment raised>
                 <div className='font-size-large font-weight-large'>
-                    {`Question ${this.state.CurrentQuestionNumber+1} / ${numberOfQuestions}`}
+                    {`Question ${cqn+1} / ${totalQuestions}`}
                 </div>
-                <Progress width={`${(this.state.CurrentQuestionNumber+1)/numberOfQuestions*100}%`} size='tiny' />
+                <Progress width={`${(cqn+1)/totalQuestions*100}%`} size='tiny' />
                 <h4>{question.statement}</h4>
                 <small className='text-primary'>Choose one correct answer.</small>
                 <div>
@@ -98,20 +90,17 @@ export class Question extends Component {
                         return (
                             <Option 
                                 light={
-                                    (!this.state.InputLocked[this.state.CurrentQuestionNumber]) ?(
-                                        this.state.UsersInput[this.state.CurrentQuestionNumber]===option.id?false:true
-                                    )
-                                    : (
-                                        !(this.state.UsersInput[this.state.CurrentQuestionNumber]===option.id) && !(option.id===question.answer.id) ? true : false
-                                    )
+                                    !isInputLocked ? 
+                                    (usersInput===option.id?false:true) :
+                                    (!(usersInput===option.id) && !(option.id===question.answer.id) ? true : false)
                                 }
-                                dark={this.state.UsersInput[this.state.CurrentQuestionNumber]===option.id?true:false} 
-                                success={(this.state.InputLocked[this.state.CurrentQuestionNumber] && question.answer.id===option.id)?true:false}
-                                danger={(this.state.InputLocked[this.state.CurrentQuestionNumber] && (this.state.UsersInput[this.state.CurrentQuestionNumber]===option.id) && !(question.answer.id===option.id)) ? true : false}
+                                dark={usersInput===option.id?true:false} 
+                                success={(isInputLocked && question.answer.id===option.id)?true:false}
+                                danger={(isInputLocked && (usersInput===option.id) && !(question.answer.id===option.id)) ? true : false}
                                 key={option.id} 
-                                hover={this.state.InputLocked[this.state.CurrentQuestionNumber]?false:true}
+                                hover={isInputLocked?false:true}
                                 onClick={(event) => {
-                                    if(this.state.InputLocked[this.state.CurrentQuestionNumber]) return false;
+                                    if(isInputLocked) return false;
                                     return this.ClickHandler(event, option);
                                 }}
                             >
@@ -119,25 +108,25 @@ export class Question extends Component {
                                 <span>{option.value}</span>
                                 <span className='float-right'>
                                     {
-                                        (this.state.InputLocked[this.state.CurrentQuestionNumber] && this.state.UsersInput[this.state.CurrentQuestionNumber]===option.id && !(option.id===question.answer.id)) ? (
-                                            <FaTimesCircle 
-                                                style={{
-                                                    color: '#842029'
-                                                }}
-                                            />
+                                        (   
+                                            isInputLocked && 
+                                            usersInput===option.id && 
+                                            !(option.id===question.answer.id)
+                                        ) ? (
+                                            <FaTimesCircle style={{color: '#842029'}} />
                                         ) : (
                                             <FaCheckCircle
                                                 style={{
                                                     color: `${
-                                                        (this.state.InputLocked[this.state.CurrentQuestionNumber]) ? (
-                                                            (this.state.UsersInput[this.state.CurrentQuestionNumber]===option.id) ? (
+                                                        (isInputLocked) ? (
+                                                            (usersInput===option.id) ? (
                                                                 (option.id===question.answer.id) ? '#0f5132' : '#842029'
                                                             ) : (
                                                                 (option.id===question.answer.id) ? '#0f5132' : '#a6a6a6'
                                                             ) 
                                                         )
                                                         : (
-                                                            this.state.UsersInput[this.state.CurrentQuestionNumber]===option.id?'#084298':'#a6a6a6'
+                                                            usersInput===option.id?'#084298':'#a6a6a6'
                                                         )
                                                     }`
                                                 }}
@@ -152,13 +141,10 @@ export class Question extends Component {
 
                 <Divider invisible/>
 
-                {(!this.state.InputLocked[this.state.CurrentQuestionNumber]) ? (
+                {(!isInputLocked) ? (
                     <Button 
-                        disabled={this.state.UsersInput[this.state.CurrentQuestionNumber]===null?true:false} 
-                        block 
-                        primary
-                        type="button"
-                        onClick={this.SubmitHandler}
+                        disabled={usersInput===null?true:false} 
+                        block primary type="button" onClick={this.SubmitHandler}
                     >
                         Check Your Answer
                     </Button>
@@ -166,15 +152,15 @@ export class Question extends Component {
                     <></>
                 )}
                 
-                {(this.state.InputLocked[this.state.CurrentQuestionNumber]) ? (
+                {(isInputLocked) ? (
                     <>
                         <Message 
-                            positive={(this.state.UsersInput[this.state.CurrentQuestionNumber]===question.answer.id) ? true : false}
-                            negative={(this.state.UsersInput[this.state.CurrentQuestionNumber]===question.answer.id) ? false : true}
+                            positive={(usersInput===question.answer.id) ? true : false}
+                            negative={(usersInput===question.answer.id) ? false : true}
                         >
                             <div className='font-size-large font-weight-bolder align-center'>
                                 {
-                                    (this.state.UsersInput[this.state.CurrentQuestionNumber]===question.answer.id) ? (
+                                    (usersInput===question.answer.id) ? (
                                         <span>
                                             <span>Correct Answer</span>
                                             <span role='img' aria-label='slightly smiling face' className='ml-medium'>🙂</span>
@@ -187,20 +173,14 @@ export class Question extends Component {
                                     )
                                 }
                             </div>
-                            <div>
-                                <p>{question.comment}</p>
-                            </div>
+                            <p>{question.comment}</p>
                         </Message>
-                        <div>
-                            <Button className='float-left' primary small style={{display: `${this.state.CurrentQuestionNumber===0?'none':'inline-block'}`}} type='button' onClick={this.GoToPreviousQuestion}>
-                                <FaAngleDoubleLeft className='mr-medium' />
-                                <span>Previous Question</span>
-                            </Button>
-                            <Button className='float-right' primary small style={{display: `${this.state.CurrentQuestionNumber===(this.props.questions.length-1)?'none':'inline-block'}`}} type='button' onClick={this.GoToNextQuestion}>
-                                <span>Next Question</span>
-                                <FaAngleDoubleRight className='ml-medium' />
-                            </Button>
-                        </div>
+                        <Button className='float-left' primary small style={{display: `${cqn===0?'none':'inline-block'}`}} type='button' onClick={this.GoToPreviousQuestion}>
+                            Previous Question
+                        </Button>
+                        <Button className='float-right' primary small style={{display: `${cqn===(totalQuestions-1)?'none':'inline-block'}`}} type='button' onClick={this.GoToNextQuestion}>
+                            Next Question
+                        </Button>
                     </>
                 ) : (
                     <></>
